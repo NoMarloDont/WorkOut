bf9e46200cdcb49682fdd287326745b31a872e1c
thomashoang <tphoang@umich.edu>
2020-11-16 21:23:30 -0800

    set up react

---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export function App() {
+    return <div>Hello World</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
