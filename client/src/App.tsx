3c7992e8a4fee50ca3cd4bda0b46124626edf079
thomashoang <tphoang@umich.edu>
2020-11-23 22:04:31 -0800

    added google sign in button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import Axios from 'axios';
+import GoogleLogin from 'react-google-login';
+import { useGoogleLogin } from 'react-google-login'
 
 export default function App(): React.ReactElement {
   const [text, setText] = useState<string>();
+  const [accessToken, setAccessToken] = useState<string>();
 
   useEffect(() => {
     Axios.get<string>('http://localhost:8000')
@@ -13,5 +16,22 @@ export default function App(): React.ReactElement {
       });
   });
 
-  return <div>{text == null ? 'Loading' : text}</div>;
+  const responseGoogle = (response) => {
+    console.log(response);
+    if(response.accessToken) {
+        setAccessToken(response.accessToken);
+    }
+    console.log(accessToken);
+  }
+
+  return <div>
+      {text == null ? 'Loading' : text}
+      <GoogleLogin
+    clientId="812267761968-lvohpt9e9uiudh8s0r9s8n6gciqjqrr5.apps.googleusercontent.com"
+    buttonText="Login"
+    onSuccess={responseGoogle}
+    onFailure={responseGoogle}
+    cookiePolicy={'single_host_origin'}
+  />
+      </div>;
 }
