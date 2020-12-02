4165ca1a7e4dbaf9ca9a418b14a79755953460ca
Charlie <charles.meighen@gmail.com>
2020-12-01 21:54:21 -0800

    added some handling for response from google login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import Axios from 'axios';
-import GoogleLogin from 'react-google-login';
-import { useGoogleLogin } from 'react-google-login'
+import GoogleLogin, {GoogleLoginResponse, GoogleLoginResponseOffline} from 'react-google-login';
 
 export default function App(): React.ReactElement {
   const [text, setText] = useState<string>();
-  const [accessToken, setAccessToken] = useState<string>();
+  const [response, setResponse] = useState<GoogleLoginResponse>();
 
   useEffect(() => {
     Axios.get<string>('http://localhost:8000')
@@ -16,22 +15,25 @@ export default function App(): React.ReactElement {
       });
   });
 
-  const responseGoogle = (response) => {
+  const responseGoogle = (response: GoogleLoginResponse | GoogleLoginResponseOffline) => {
     console.log(response);
-    if(response.accessToken) {
-        setAccessToken(response.accessToken);
+    if (!('code' in response)) {
+      setResponse(response);
     }
-    console.log(accessToken);
-  }
+  };
 
-  return <div>
+  return (
+    <div>
       {text == null ? 'Loading' : text}
       <GoogleLogin
-    clientId=""
-    buttonText="Login"
-    onSuccess={responseGoogle}
-    onFailure={responseGoogle}
-    cookiePolicy={'single_host_origin'}
-  />
-      </div>;
+        clientId=""
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
+
+      {response ? `Logged In: ${response.getBasicProfile().getGivenName()}` : 'Not Logged In'}
+    </div>
+  );
 }
